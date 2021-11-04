56e981589cb0e4d7e1ec096848170b0a49b21705
ivunortiz <ivan.ortiz1@ingka.ikea.com>
2021-11-03 23:58:42 -0400

    Completed the navbar. Small bug still needs to be fixed.

--- Navbar.js
+++ Navbar.js
@@ -1,6 +1,18 @@
 const btnToggle = document.querySelector(".nav-toggle");
-const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenu = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links li");
 
 btnToggle.addEventListener("click", function() {
-    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = '';
+        } else {
+
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+        }
+    });
+
+    btnToggle.classList.toggle("toggle");
 })
