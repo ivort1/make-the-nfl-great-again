a445378a2762a55a6ba5d5b5575797e42255e856
ivort1 <ivan.ortiz1@ingka.ikea.com>
2023-08-29 11:08:46 -0700

    Configured env variables.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    NEXT_PUBLIC_LEAGUE_UID: process.env.NEXT_PUBLIC_LEAGUE_UID
+  },
     images: {
         domains: ['sleepercdn.com', 's.yimg.com', 'uaipdvtubwrickybbufs.supabase.co'],
       },
