519e46fc0e6411d02a12f645031e2c6d9bb90e54
ivort1 <ivan.ortiz1@ingka.ikea.com>
2023-08-29 11:17:14 -0700

    Added projectId in Sanity

--- sanity/env.js
+++ sanity/env.js
@@ -1,4 +1,4 @@
 export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-08-24';
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'u0mvwzej';
 export const useCdn = false;
