de8219925ce8d09fe85e0ccbc46c6b37b9ac4bba
ivort1 <ivan.ortiz1@ingka.ikea.com>
2023-08-30 12:41:18 -0700

    Added next validation again

--- sanity/sanity-utils.js
+++ sanity/sanity-utils.js
@@ -2,27 +2,39 @@ import { createClient, groq } from 'next-sanity';
 import clientConfig from "./config/client-config";
 
 export async function getAnnouncements() {
+    const revalidate = 30;
+
     return createClient(clientConfig).fetch(
-      groq`*[_type == "announcement"]`
+      groq`*[_type == "announcement"]`,
+      {next: {revalidate}}
     )
 }
 
 export async function getAnnouncement(slug) {
+    const revalidate = 30;
+
     return createClient(clientConfig).fetch(
       groq`*[_type == "announcement" && slug.current == $slug][0]`,
-      { slug }
+      {slug},
+      {next: {revalidate }}
     )
 }
 
 export async function getPosts() {
+    const revalidate = 30;
+
     return createClient(clientConfig).fetch(
-      groq`*[_type == "rankings" || _type == "article" || _type == "announcement"]`
+      groq`*[_type == "rankings" || _type == "article" || _type == "announcement"]`,
+      {next: {revalidate }}
     )
 }
 
 export async function getPost(slug) {
+    const revalidate = 30;
+    
     return createClient(clientConfig).fetch(
       groq`*[(_type == "rankings" || _type== "article" || _type== "announcement") && slug.current == $slug][0]`,
-      { slug }
+      {slug},
+      {next: {revalidate }}
     )
 }
