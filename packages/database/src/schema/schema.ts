62d20881d5a065b13b169817e334a5289e3d89ac
Ivan <ivan@pop-os.local>
2026-01-23 13:15:32 -0800

    Completed the Drizzle schema for the database. Pushed the schema to the database and it was populated successfully.

--- packages/database/src/schema/schema.ts
+++ packages/database/src/schema/schema.ts
@@ -1,4 +1,15 @@
-import { decimal, integer, pgEnum, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+    decimal,
+    index,
+    integer,
+    pgEnum,
+    pgTable,
+    text,
+    timestamp,
+    uniqueIndex,
+    uuid,
+    varchar,
+} from "drizzle-orm/pg-core";
 
 // ----- Owner -----
 export const owner = pgTable("owner", {
@@ -10,15 +21,61 @@ export const owner = pgTable("owner", {
     updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
 });
 
-
 // ----- Season -----
 export const platformEnum = pgEnum("platform_type", ["sleeper", "espn", "yahoo", "manual"]);
 
 export const season = pgTable("season", {
     id: uuid("id").primaryKey().defaultRandom(),
     year: integer("year").notNull().unique(),
-    platform: platformEnum().default("sleeper"),
+    platform: platformEnum("platform").default("sleeper"),
     playoffSlots: integer("playoff_slots").notNull(),
     leagueFee: decimal("league_fee", { precision: 10, scale: 2 }).default("0.00"),
     createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
 });
+
+// ----- Team -----
+export const team = pgTable("team", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    ownerId: uuid("owner_id").notNull().references(() => owner.id, {onDelete: "cascade" }),
+    seasonId: uuid("season_id").notNull().references(() => season.id, { onDelete: "cascade" }),
+    teamName: varchar("team_name").notNull(),
+    teamPhotoUrl: text("team_photo_url"),
+    regularSeasonStanding: integer("regular_season_standing"),
+    finalStanding: integer("final_standing"),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow()
+}, (table) => [
+    uniqueIndex("unq_idx_team_owner_id_season_id").on(table.ownerId, table.seasonId),
+    index("idx_team_season_id").on(table.seasonId)
+]);
+
+// ----- Matchup -----
+export const matchupTypeEnum = pgEnum("matchup_type", ["regular", "playoff", "consolation"]);
+
+export const matchup = pgTable("matchup", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    seasonId: uuid("season_id").notNull().references(() => season.id),
+    teamId: uuid("team_id").notNull().references(() => team.id),
+    week: integer("week").notNull(),
+    matchupGroupId: integer("matchup_group_id").notNull(),
+    points: decimal("points", { precision: 5, scale: 2 }).default("0.00").notNull(),
+    type: matchupTypeEnum("type").default("regular"),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow()
+}, (table) => [
+    uniqueIndex("unq_idx_matchup_team_id_week").on(table.teamId, table.week),
+    index("idx_matchup_team_id").on(table.teamId),
+    index("idx_matchup_season_week").on(table.seasonId, table.week)
+])
+
+// ----- Sync Log -----
+export const syncLog = pgTable("synclog", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    seasonYear: integer("season_year").notNull(),
+    seasonWeek: integer("season_week").notNull(),
+    syncedAt: timestamp("synced_at", { withTimezone: true }).defaultNow(),
+    status: varchar("status", { length: 50 }).notNull(),
+    errorMessage: text("error_message"),
+    matchupsAdded: integer("matchups_added").default(0)
+}, (table) => [
+    uniqueIndex("unq_idx_sync_log_season_year_season_week").on(table.seasonYear, table.seasonWeek)
+])
