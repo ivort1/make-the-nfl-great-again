cf8d5c098401b3fb2fd9dca3678e4d93cdcc4520
Ivan <ivan@pop-os.local>
2026-01-24 10:20:33 -0800

    Added a variant of code in /packages/database/src/index.ts

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -7,3 +7,21 @@ export const createDb = (connectionString: string) => {
 
 export type Database = ReturnType<typeof createDb>;
 export * from './schema/schema';
+
+// import 'dotenv/config';
+// import { Pool } from 'pg';
+// import { drizzle } from 'drizzle-orm/node-postgres';
+// import * as schema from './schema/schema';
+
+// 1. Setup the Postgres Connection Pool
+// This efficiently manages multiple connections from the API to the DB
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL!,
+// });
+
+// 2. Initialize Drizzle with the schema
+// Passing { schema } enables the "query" builder (db.query.users.findMany)
+// export const db = drizzle(pool, { schema });
+
+// 3. Export the schema so other packages can use the types
+// export * from './schema/schema';
